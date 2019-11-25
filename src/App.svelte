56f446c8219e44a00812557f924744aa0cb69172
Gil Fewster <hello@gilfewster.com>
2019-11-25 13:42:23 +1100

    basic front end for protoype/demo

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,65 @@
 <script>
   import { onMount } from "svelte";
   import Query from "./airtable/Query";
-  let titles;
+  import SparkView from "./SparkView.svelte";
+
+  let sparks = [];
+  let currentSpark = {};
+  let isLoading = false;
 
   const defaultQuery = {
     table: "Spark",
-    maxRecords: 3,
-    fields: ["Title", "Content", "Tags", "Contributor"]
+    maxRecords: 500,
+    fields: ["title", "content"]
+  };
+
+  const loadSparks = () => {
+    return new Promise(async (resolve, reject) => {
+      isLoading = true;
+
+      const query = new Query(defaultQuery);
+      const res = await fetch("/api/recordIdList", {
+        method: "POST",
+        body: JSON.stringify(query)
+      });
+
+      const result = await res.text();
+      const data = JSON.parse(result);
+      isLoading = false;
+      resolve(data);
+    });
+  };
+
+  const getRandomSpark = async () => {
+    if (sparks.length < 1) {
+      sparks = await loadSparks();
+    }
+    const rnd = Math.round(Math.random() * (sparks.length - 1));
+    currentSpark = { ...sparks[rnd] };
   };
 
   onMount(async () => {
-    const query = new Query(defaultQuery);
-    const res = await fetch("/api/recordIdList", {
-      method: "POST",
-      body: JSON.stringify(query)
-    });
-    titles = await res.text();
+    await loadSparks();
+    getRandomSpark();
   });
 </script>
 
 <main>
-  <h2>Titles of first three records:</h2>
-  <p>{titles ? titles : 'Loading titles...'}</p>
+  {#if isLoading}
+    <p>...loading...</p>
+  {:else}
+    <div class="card">
+      <h1>{currentSpark.title}</h1>
+      <p>{currentSpark.content}</p>
+      <a
+        class="btn"
+        on:click={e => {
+          e.preventDefault();
+          getRandomSpark();
+        }}
+        href="/">
+        ☞
+      </a>
+    </div>
+  {/if}
 </main>
---
+++ src/SparkView.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data = {
+    title: "Title",
+    content: "lorem ipsum dolore etc"
+  };
+</script>
+
+<div class="spark">
+  <h1>{data.title}</h1>
+  <p>{data.content}</p>
+</div>
