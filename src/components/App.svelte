285bd8847ca83efc467365003023bbe06906073d
Gil Fewster <hello@gilfewster.com>
2019-12-11 16:33:00 +1100

    moved Svelte components to /components dir inside src for neatness

---
+++ src/components/App.svelte
@@ -0,0 +1,86 @@
+<script>
+  import { onMount } from "svelte";
+  import TABLE from "../data/tables";
+  import Query from "../data/Query";
+  import Spark from "../data/Spark";
+  import fetchRecord from "../db/fetchRecord";
+  import fetchIndex from "../db/fetchIndex";
+
+  import SparkView from "./SparkView.svelte";
+
+  let sparks = [];
+  let currentSpark;
+  let isLoading = false;
+
+  const getRandomSpark = async () => {
+    if (sparks.length > 0) {
+      const rnd = Math.round(Math.random() * (sparks.length - 1));
+      const id = sparks[rnd];
+
+      const endpoint = `/spark/${id}`;
+      const params = {
+        fields: [
+          TABLE.SPARK.FIELDS.TITLE,
+          TABLE.SPARK.FIELDS.CONTENT,
+          TABLE.SPARK.FIELDS.TAGS,
+          TABLE.SPARK.FIELDS.ACTIONS
+        ]
+      };
+
+      const query = new Query(endpoint, params);
+
+      try {
+        isLoading = true;
+        const { records, message, error } = await fetchRecord(query);
+        currentSpark = records[0];
+      } catch (e) {
+        console.log(e);
+      }
+      isLoading = false;
+    } else {
+      console.log("Can't display random spark :: No records available");
+    }
+  };
+
+  const loadIndex = async () => {
+    isLoading = true;
+    const { records, message, error } = await fetchIndex();
+    isLoading = false;
+
+    if (error || records.length == 0) {
+      logIndexLoadError(error);
+    }
+    if (records.length > 0) {
+      sparks = records.map(record => record);
+      getRandomSpark();
+    }
+  };
+
+  const logIndexLoadError = (error = null) => {
+    const msg =
+      "Error loading record index." + (error ? `\n > ${error}` : null);
+    console.log(msg);
+  };
+
+  onMount(() => {
+    loadIndex();
+  });
+
+  const handleBtnClick = e => {
+    e.preventDefault();
+    getRandomSpark();
+  };
+</script>
+
+<style global lang="scss">
+  @import "../scss/global.scss";
+</style>
+
+<main>
+  {#if isLoading}
+    <p>...loading...</p>
+  {:else if currentSpark != null}
+    <SparkView data={currentSpark || null} />
+    <a class="btn" on:click={handleBtnClick} href="/">☞</a>
+  {/if}
+</main>
---
+++ src/components/SparkView.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Spark from "../data/Spark";
+  export let data = new Spark();
+</script>
+
+<style>
+
+</style>
+
+<div class="spark">
+  <h1>{data.title}</h1>
+  <ul data-actions>
+    {#each data.actions as action}
+      <li>{action}</li>
+    {/each}
+
+  </ul>
+  <p>{data.content}</p>
+  <ul data-tags>
+    {#each data.tags as tag}
+      <li>{tag}</li>
+    {/each}
+  </ul>
+</div>
