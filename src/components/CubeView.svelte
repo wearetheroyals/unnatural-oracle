8b4636803e73b3ad334d9cdc17f1ce80a3cb981a
Gil Fewster <hello@gilfewster.com>
2020-02-11 17:15:01 +1100

    css full size

---
+++ src/components/CubeView.svelte
@@ -0,0 +1,34 @@
+<style src="../scss/cube.scss"></style>
+
+<script>
+  import { afterUpdate } from "svelte";
+  import Spark from "../data/Spark";
+
+  export let data = new Spark();
+  
+  const containers = {};
+
+  afterUpdate(() => {
+    //
+  });
+</script>
+
+<div class="card">
+
+  <header bind:this={containers.title}>
+    <h1 class="fittext">{data.title}</h1>
+  </header>
+
+  <section bind:this={containers.content} class="content">
+    <p class="">{data.content}</p>
+  </section>
+
+  <section class="meta">
+    <ul data-tags class="labels">
+      {#each data.tags as tag}
+        <li>{tag}</li>
+      {/each}
+    </ul>
+  </section>
+
+</div>
