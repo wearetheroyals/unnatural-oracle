740c2a306c6d6f3cd016ce3fe742d743ac991207
Gil Fewster <hello@gilfewster.com>
2020-02-14 13:39:54 +1100

    loader animation

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -6,14 +6,17 @@
   import { randomRangeInt } from "../util/randomRange.js";
   import { isLoading, useMockData } from '../store.js';  
   
-  // import View from "./OracleView.svelte";
+
   import Eyeball from "./Eyeball.svelte";
-  // import View from "./OracleView.svelte";
+  import Loading from "./Loading.svelte";
+
 
   let currentSpark;
   let apiConn;
+  let container
+  let activePalette = 0;
   
-  useMockData.set(true);
+  useMockData.set(false);
 
   onMount(() => init());
 
@@ -31,6 +34,7 @@
   const fetchRandomItem = async () => {
     try {
       currentSpark = await apiConn.fetchItem();
+      cyclePalette(container);
     } catch (e) {
       handleApiError(e)
     }
@@ -45,24 +49,76 @@
     console.error(e);
   }
 
+
+  // ****************************
+
+  const VARIANTS = {
+      BASE: 'base',
+      DARKER: 'darker',
+      LIGHTER: 'lighter'
+    }
+
+    const palettes = [
+      {
+      // blue
+      [VARIANTS.BASE]: '#217afa',
+      [VARIANTS.DARKER]: '#002070',
+      [VARIANTS.LIGHTER]: '#d0f0ff',
+    },
+    {
+      // green
+      [VARIANTS.BASE]: '#20a050',
+      [VARIANTS.DARKER]: '#107030',
+      [VARIANTS.LIGHTER]: '#c0e0c0',
+    },
+    {
+      // yellow
+      [VARIANTS.BASE]: '#ffe040',
+      [VARIANTS.DARKER]: '#ffc000',
+      [VARIANTS.LIGHTER]: '#fff0c0',
+    },
+    {
+      // red
+      [VARIANTS.BASE]: '#ff4020',
+      [VARIANTS.DARKER]: '#d02030',
+      [VARIANTS.LIGHTER]: '#ffd0de',
+    },
+    ]
+
+
+    const getCol = (variant) => {
+      return palettes[activePalette][variant];
+    }
+
+    const cyclePalette = (el) => {
+      activePalette = activePalette +1 >= palettes.length ? 0 : activePalette+1;
+      el.style.setProperty('--col-base', getCol(VARIANTS.BASE));
+      el.style.setProperty('--col-darker', getCol(VARIANTS.DARKER));
+      el.style.setProperty('--col-lighter', getCol(VARIANTS.LIGHTER));
+    }
+
+
+
 </script>
 
 <main>
-  {#if $isLoading}
-    <p>...loading...</p>
-  {:else if currentSpark != null}
     <div class="ratio">
-    <div id='spark'>
+    <div id='spark' bind:this={container} on:click={fetchRandomItem}>
       <section class="oracle">
         <Eyeball />
       </section>
-      <section class="content">
-        <p>{currentSpark.fields.content}</p>
+      <section class="{$isLoading ? 'loading' : 'content'}">
+        {#if $isLoading}     
+          <Loading />
+        {:else}
+          {#if currentSpark != null}
+          <p>{currentSpark.fields.content}</p>
+          {/if}
+        {/if}
       </section>
       <section class="brand">
         Royals
       </section>
     </div>
     </div>
-  {/if}
 </main>
---
+++ src/components/Loading.svelte
@@ -0,0 +1,20 @@
+<script>
+    // import LoadingGif from '../assets/loading.gif';
+    // const src = 'loading.gif'
+</script>
+<style>
+    div{
+        padding: 0;
+        margin: 0;
+        width: 100%;
+        height: 100%;
+        background: url('loading.gif');
+        background-size: cover;
+        background-position: 50% 50%;
+        /* z-index: 100; */
+        background-color: rgba(0,0,0,.75);
+    }
+     
+</style>
+<div>
+</div>
